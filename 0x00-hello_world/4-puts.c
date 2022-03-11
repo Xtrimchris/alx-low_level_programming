e503ed03c9e403e069904136d65d17f36cf53875
Xtrimchris <ashinze212@gmail.com>
2022-03-11 01:01:36 +0100

    Hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h    //Importation of Standard input/output library
+
+int main ()         //This is the entry point of tyhe program
+                    //Executions begins
+{
+    puts("\"Programming is like building a multi lingual puzzle");
+        //Sending output to the display via stdio.h
+
+    return 0;   // Program stops and terminate
+}
