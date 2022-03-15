0ccb001f5574af70a3929c8b6674b083c83373bb
Xtrimchris <ashinze212@gmail.com>
2022-03-15 22:10:34 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("_putchar\n");
+    return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# Functions, Nested Loops in C Language
+A deep dive into functions and nested loops in C programming language
